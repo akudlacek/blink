16d3bc3706af3d0cda8d50f0d2e89320d5bdd419
akudlacek <7029639+akudlacek@users.noreply.github.com>
2020-04-09 17:18:23 -0500

    Added idle value changed to max of 16 blinks previously was one less than num of bits removed while loop in bit search fixed possible wrap bug
    NOTE: if the idle value is one, the first blink will look like idle
    
    Signed-off-by: akudlacek <7029639+akudlacek@users.noreply.github.com>

--- blink.c
+++ blink.c
@@ -22,6 +22,7 @@ void blink_get_confg_defaults(blink_conf_t * const conf)
 	conf->tick_ptr             = 0;
 	conf->ticks_per_time_unit  = 0;
 	conf->blink_separation     = 0;
+	conf->idle_value           = 0;
 }
 
 /******************************************************************************
@@ -36,7 +37,7 @@ void blink_init(blink_inst_t * const inst, blink_conf_t const conf)
 	inst->state         = GET_NEXT_SEQ;
 	
 	inst->blink_bit     = 0;
-	inst->num_of_blinks = 0;
+	inst->bit_number    = 0;
 	
 	inst->out           = 0;
 	inst->count         = 0;
@@ -51,83 +52,106 @@ void blink_init(blink_inst_t * const inst, blink_conf_t const conf)
 ******************************************************************************/
 uint8_t blink_task(blink_inst_t * const inst)
 {
-	if((*inst->conf.tick_ptr - inst->last_tick) >= (inst->conf.ticks_per_time_unit))
+	//if no blink bit set and done blinking go to idle value
+	if((inst->blink_bit == 0) && (inst->state == GET_NEXT_SEQ))
+	{
+		inst->out = 0;
+		return (inst->conf.idle_value & 1u);
+	}
+	//run state machine
+	else
 	{
 		switch(inst->state)
 		{
 			case GET_NEXT_SEQ:
-			if(inst->blink_bit > 1)
+			//if no blink bit set in this bit position, increment
+			if(((inst->blink_bit >> inst->bit_number) & 1u) == 0)
 			{
-				/*there is a blink sequence to start*/
+				inst->bit_number++;
 				
-				/*Find next set bit*/
-				while((inst->blink_bit & (1 << inst->num_of_blinks)) == 0)
+				//wrap
+				if(inst->bit_number > BLINKS_MAX)
 				{
-					inst->num_of_blinks++;
-					
-					if(inst->num_of_blinks >= 32)
-					{
-						inst->num_of_blinks = 0;
-					}
-					
-					/*Just in case*/
-					if(inst->blink_bit <= 1)
-					{
-						break;
-					}
+					inst->bit_number = 0;
 				}
 				
-				inst->count = 0;
-				inst->out = 1;
-				inst->state = BLINKING;
+				inst->out = 0;
+				
+				/*stay in GET_NEXT_SEQ state*/
 			}
+			//live bit found
 			else
 			{
-				/*stay in GET_NEXT_SEQ state*/
-				inst->out = 0;
+				inst->count     = 0;                      //reset count
+				inst->out       = 1;                      //start first blink
+				inst->last_tick = *inst->conf.tick_ptr;   //reset tick time
+				inst->state     = BLINKING;               //go to blink state
 			}
 			break;
 			
 			case BLINKING:
-			inst->count += inst->out; //adds to count if out is 1
-			inst->out ^= 1;           //toggles output
-			
-			if(inst->count >= inst->num_of_blinks)
+			if((*inst->conf.tick_ptr - inst->last_tick) >= (inst->conf.ticks_per_time_unit))
 			{
-				/*Hit the num of blinks*/
+				inst->count += inst->out; //adds to count if out is 1
+				inst->out   ^= 1;         //toggles output
 				
-				/*Advance to the next sequence*/
-				inst->num_of_blinks++;
+				//if hit number of blinks
+				if(inst->count > inst->bit_number)
+				{
+					inst->bit_number++; //Advance to the next sequence, so it does not get stuck running same bit
+					
+					//wrap
+					if(inst->bit_number > BLINKS_MAX)
+					{
+						inst->bit_number = 0;
+					}
+					
+					//if no separation go to GET_NEXT_SEQ state
+					if(inst->conf.blink_separation == 0)
+					{
+						inst->state = GET_NEXT_SEQ;
+					}
+					else
+					{
+						/*Separation between sequences is 1 or larger*/
+						inst->count = 0;
+						inst->out   = 0;
+						inst->state = SEQ_SEPARATION;
+					}
+				}
 				
-				if(inst->conf.blink_separation == 0)
+				inst->last_tick = *inst->conf.tick_ptr;
+			}
+			break;
+				
+			case SEQ_SEPARATION:
+			if((*inst->conf.tick_ptr - inst->last_tick) >= (inst->conf.ticks_per_time_unit))
+			{
+				inst->count++;
+				
+				if(inst->count >= inst->conf.blink_separation)
 				{
 					inst->state = GET_NEXT_SEQ;
 				}
-				else
-				{
-					/*Separation between sequences is 1 or larger*/
-					inst->count = 0;
-					inst->out   = 0;
-					inst->state = SEQ_SEPARATION;
-					
-				}
+				
+				inst->last_tick = *inst->conf.tick_ptr;
 			}
 			break;
 			
-			case SEQ_SEPARATION:
-			inst->count++;
-			
-			if(inst->count >= inst->conf.blink_separation)
-			{
-				inst->state = GET_NEXT_SEQ;
-			}
+			default:
+			//something bad happened
+			inst->state         = GET_NEXT_SEQ;
+			inst->blink_bit     = 0;
+			inst->bit_number    = 0;
+			inst->out           = 0;
+			inst->count         = 0;
+			inst->last_tick     = *inst->conf.tick_ptr;
+			return (inst->conf.idle_value & 1u);
 			break;
-		}
-		
-		inst->last_tick = *inst->conf.tick_ptr;
+		} /* switch */
 	}
 	
-	return inst->out;
+	return (inst->out & 1u);
 }
 
 /******************************************************************************
@@ -135,7 +159,7 @@ uint8_t blink_task(blink_inst_t * const inst)
 *
 *  \note sets num of blinks by bit position num_blinks = 0 turns all off
 ******************************************************************************/
-void blink_set(blink_inst_t * const inst, uint8_t const num_blinks, uint8_t const on_off)
+void blink_set(blink_inst_t * const inst, uint8_t num_blinks, uint8_t const on_off)
 {
 	if(num_blinks == 0)
 	{
@@ -143,6 +167,8 @@ void blink_set(blink_inst_t * const inst, uint8_t const num_blinks, uint8_t cons
 		
 		return;
 	}
+	
+	num_blinks--;
 	
 	if(on_off == 1)
 	{
--- blink.h
+++ blink.h
@@ -16,6 +16,8 @@
 /**************************************************************************************************
 *                                             DEFINES
 *************************************************^************************************************/
+#define BLINKS_MAX 16
+
 typedef enum blink_state_t
 {
 	GET_NEXT_SEQ,
@@ -25,9 +27,10 @@ typedef enum blink_state_t
 
 typedef struct blink_conf_t
 {
-	const volatile uint32_t *tick_ptr;
-	uint32_t ticks_per_time_unit;  //One blink is on for time unit and off for another
-	uint32_t blink_separation;     //Blink separation is 1 time unit long and provides some dead time between blinks
+	const volatile uint32_t * tick_ptr;
+	uint32_t ticks_per_time_unit;          //One blink is on for time unit and off for another
+	uint16_t blink_separation;             //Blink separation is 1 time unit long and provides some dead time between blinks
+	uint8_t idle_value;                    //Value of output when there is no blink
 } blink_conf_t;
 
 typedef struct blink_inst_t
@@ -35,11 +38,11 @@ typedef struct blink_inst_t
 	blink_conf_t conf;
 	blink_state_t state;
 	
-	uint32_t blink_bit;
-	uint8_t num_of_blinks;
+	uint16_t blink_bit;
+	uint8_t bit_number;
 	
 	uint8_t out;
-	uint32_t count;
+	uint8_t count;
 	
 	uint32_t last_tick;
 } blink_inst_t;
@@ -51,7 +54,7 @@ typedef struct blink_inst_t
 void    blink_get_confg_defaults(blink_conf_t * const conf);
 void    blink_init(blink_inst_t * const inst, blink_conf_t const conf);
 uint8_t blink_task(blink_inst_t * const inst);
-void    blink_set(blink_inst_t * const inst, uint8_t const num_blinks, uint8_t const on_off);
+void    blink_set(blink_inst_t * const inst, uint8_t num_blinks, uint8_t const on_off);
 
 
 #endif /* BLINK_H_ */
